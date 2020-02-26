4f80c3c963a35c10d1c5db1ab70ca56542d867cf
Eduarddo <rosana.nelli@gmail.com>
2020-02-26 07:23:57 -0300

    Inserir e alterar cores

--- Class/Cores.php
+++ Class/Cores.php
@@ -1,4 +1,5 @@
 <?php 
+//Classe cores utilizada tanto pelos veículos como por implementos
 class Cores {
 
 	private $idcores;
@@ -18,29 +19,74 @@ class Cores {
 		$this->descores = $descores;
 	}
 
-	public function loadById($id){
+	public function loadById($id){   //carrega apenas uma única descrição de cor com base no id
 
 		$sql = new Sql();
 		$results = $sql->select("SELECT * FROM cores WHERE idcores = :ID", array(":ID"=>$id));
 
 		if(count($results)>0){
-			$row = $results[0];
-
-			$this->setIdcores($row['idcores']);
-			$this->setDescores($row['descores']);
+			$this->setData($results[0]);
 		}
-
-
 	}
 
-	public static function getList(){
+	public static function getList(){    //lista todas as cores da tabela
 
 		$sql = new Sql();
 		return $sql->select ("SELECT * FROM cores ORDER BY idcores");
 
 	}
+
+	public static function search($cor){   //carrega uma cor tomando por base a descrição ou parte dela
+
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM cores WHERE descores LIKE :SEARCH ORDER BY descores", array(
+			':SEARCH'=>"%".$cor."%"	
+		));
+		if (count($results)>0){
+			return $results;
+		}else{
+			return "Registro inexistente!";
+		}
+	}
+
+	public function setData($data){     //esta função apenas faz os set's 
+
+		$this->setIdcores($data['idcores']);
+		$this->setDescores($data['descores']);
+		}
+
+
+	public function insert(){    //utilizada para fazer a gravação de um novo registro. Utiliza uma procedure gravada diretamente no banco chamada sp_cores_insert. Retorna o registro que foi gravado
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_cores_insert(:COR)", array(
+				':COR'=>$this->getDescores()			
+		));
+		if (count($results)>0){
+
+			$this->setData($results[0]);	
+		}
+		
+
+	}
+
+	public function update($cor){   //atualiza a descrição da cor
+
+		$this->setDescores($cor);
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE cores SET descores = :COR WHERE idcores = :ID", array( 
+			':COR'=>$this->getDescores(),
+			':ID'=>$this->getIdcores()
+		));
+
+
+
+	}
 	
-	public function __toString(){
+	public function __toString(){    //quando for dado o comando echo no objeto, transforma-o em um json
 
 		return json_encode(array(
 			"idcores"=>$this->getIdcores(),
--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <?php 	
-$core = 1;
+$core = "Vermelho Intenso";
 require_once ("config.php");
 /*
 // pesquisa por um unico id
 $cor = new Cores();
-
 $cor->loadById($core);
-echo $cor;*/
+echo $cor;
 
 //lista toda a tabela
-
 $cores = Cores::getList();
 echo json_encode($cores);
+
+//carrega uma lista de cores pela descrição
+//$search = Cores::search($core);
+//echo json_encode($search);
+
+//Insere uma nova cor na tabela
+$cor = new Cores();
+$cor->setDescores($core);
+$cor->insert();
+echo $cor;*/
+
+$cor = new Cores();
+
+$cor->loadById(4);
+$cor->update("Branco e Cinza");
+echo $cor;
 
  ?>
